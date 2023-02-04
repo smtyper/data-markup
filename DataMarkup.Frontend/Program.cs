db5e5735f082cda19fd22c82fc8ba0970b13bf2b
Artem <smtyper.contact@gmail.com>
2023-02-04 10:31:23 +0300

    Clear frontend project

--- DataMarkup.Frontend/Program.cs
+++ DataMarkup.Frontend/Program.cs
@@ -1,4 +1,3 @@
-using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DataMarkup.Frontend;
@@ -8,18 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddAuthorizationCore();
-// builder.Services.AddScoped<ApiAuthenticationStateProvider>();
-// builder.Services.AddScoped<AuthService>();
 
-builder.Services.AddHttpClient<DataMarkupClient>()
-    .ConfigureHttpClient(client =>
-    {
-        client.BaseAddress = new Uri("https://localhost:5001/api/");
-        client.DefaultRequestHeaders.Add("Accept", "*/*");
-        client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
-        client.DefaultRequestHeaders.Add("Connection", "keep-alive");
-    });
+
+// builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
